2ccc5bce66f8b8b73b9a1c2a16026adbb9bfbf88
szaboakosdaniel <szaboakosdaniel@gmail.com>
2023-08-27 15:08:47 +0200

    Changed to Private variables
    Changed to Private variables
    -Getter ans Setter Methods implemented

--- Server/Statemachine.py
+++ Server/Statemachine.py
@@ -34,7 +34,7 @@ class BrowserControlMachine(StateMachine):
     cycle = c | d | e
 
 #Function to get Server IP
-    def get_internal_ip():
+    def get_internal_ip(self):
         try:
             internal_ip = socket.gethostbyname(socket.gethostname())
             print(internal_ip)
@@ -44,13 +44,47 @@ class BrowserControlMachine(StateMachine):
             return None
         
         
-#Variables
-    server_ip=get_internal_ip()
-    server_port = 12345
-    client_socket=None
-    received_message="NULL"
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Private variables
+    def __init__(self):
+        self.server_ip=self.get_internal_ip()
+        self.server_port = 12345
+        self.client_socket=None
+        self.received_message="NULL"
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.on_enter_START()
+#Setters
+    def set_server_ip(self, value):
+        self.server_ip = value
 
+    def set_server_port(self, value):
+        self.server_port = value
+
+    def set_client_socket(self, value):
+        self.client_socket = value
+
+    def set_received_message(self, value):
+        self.received_message = value
+
+    def set_server_socket(self, value):
+        self.server_socket = value
+
+#Getters
+    def get_server_ip(self):
+        return self.server_ip
+
+    def get_server_port(self):
+        return self.server_port
+
+    def get_client_socket(self):
+        return self.client_socket
+
+    def get_received_message(self):
+        return self.received_message
+
+    def get_server_socket(self):
+        return self.server_socket
+
+#Check TCP/IP Connection
     def check_connection(self):
         print("1")
         if self.client_socket is None:
@@ -73,11 +107,6 @@ class BrowserControlMachine(StateMachine):
 #INIT
     def on_enter_INIT(self):
         print("Init state...")
-        #self.server_ip=self.get_internal_ip()
-        #self.server_port = 12345
-        #self.client_socket=None
-        #self.received_message="NULL"
-        #self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((self.server_ip, self.server_port))
         self.on_enter_WAIT_FOR_CONNECTION()
      
@@ -110,17 +139,16 @@ class BrowserControlMachine(StateMachine):
             except Exception as e:
                 print("Kivetel keletkezett:", e)
                 self.client_socket.close()
-                #self.server_socket.close()
                 self.on_enter_WAIT_FOR_CONNECTION()
 
     #DECODE_MESSAGE
+    #TCP/IP socket encrypton need to be implemented
     def on_enter_DECODE_MESSAGE(self):
         print("Üzenet dekodolasa...")
         if self.received_message=="hello\n":
             print("Üzenet erkezett:")#, self.no_device_foundreceived_message)
             self.on_enter_EXECUTE_COMMAND()
         else:
-            #self.trigger(self.events.not_valid_message)
             print("Ez nem üzenet...")
 
  
