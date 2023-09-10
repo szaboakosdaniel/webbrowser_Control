86b8d4c6307aa5b2248c9997045ccec67b904dc4
szaboakosdaniel <szaboakosdaniel@gmail.com>
2023-09-10 19:18:01 +0200

    Client updated
    -Client  search server on local network
    -Youtube specif buttons implemented

--- Client/MyApplication6/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Client/MyApplication6/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,58 +1,24 @@
 package com.example.myapplication;
 
-/*
-import androidx.appcompat.app.AppCompatActivity;
-import java.net.*; //TCP-IP socket communication
-import java.io.*;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        String serverIP ="192.168,1,5";
-        int serverPort =12345;
-        try {
-            Socket socket = new Socket(serverIP, serverPort);
-
-            OutputStreamWriter out = new OutputStreamWriter(socket.getOutputStream());
-            BufferedWriter writer = new BufferedWriter(out);
-            String message = "hello";
-            writer.write(message);
-            writer.newLine();
-            writer.flush();
-            //System.out.println("Üzenet elküldve: " + message);
-
-            writer.close();
-            out.close();
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //setContentView(R.layout.activity_main);
-    }
-}
-*/
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.net.*;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.net.Socket;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
-
-import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.util.Enumeration;
-
+import android.util.Log;
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     private volatile boolean isRunning = false;
-    String serverIP = "192.168.1.5"; // Pontokkal elválasztva
+    //String serverIP = "192.168.1.5"; // Pontokkal elválasztva
     int serverPort = 12345;
     Socket socket;
     OutputStreamWriter out;
@@ -64,11 +30,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         Button button= (Button) findViewById(R.id.button);
         Button button2= (Button) findViewById(R.id.button2);
@@ -76,20 +40,80 @@ public class MainActivity extends AppCompatActivity {
         Button button4= (Button) findViewById(R.id.button4);
         Button button5= (Button) findViewById(R.id.button5);
         EditText url =(EditText) findViewById(R.id.url);
+
+
+        // Example log messages
+        Log.d(TAG, "Debug message");
+        Log.e(TAG, "Error message");
+        Log.i(TAG, "Info message");
+        Log.v(TAG, "Verbose message");
+        Log.w(TAG, "Warning message");
         // Hálózati műveleteket nem végezhetünk a fő szálon
+
 
           networkThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                if (serverIP != null) {
+                ///////////////////
+                String myIP=null;
+                try {
+                    Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+                    while (networkInterfaces.hasMoreElements()) {
+                        NetworkInterface networkInterface = networkInterfaces.nextElement();
+                        Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+                        while (inetAddresses.hasMoreElements()) {
+                            InetAddress inetAddress = inetAddresses.nextElement();
+                            if (!inetAddress.isLoopbackAddress() && !inetAddress.isLinkLocalAddress()) {
+                               // System.out.println("Device IP Address: " + inetAddress.getHostAddress());
+                                if(inetAddress instanceof Inet4Address) {
+                                    myIP = inetAddress.getHostAddress();
+                                    Log.i("KAP", myIP);
+                                    System.out.println(myIP);
+                                }
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+                String subnet = null;
+                String[] octets = myIP.split("\\.");
+
+                // Ensure that there are at least 3 octets before removing the last one
+                if (octets.length >= 3) {
+                    // Remove the last octet
+                     subnet = String.join(".", octets[0], octets[1], octets[2]);
+                    System.out.println("Modified IP Address: " + subnet);
+                } else {
+                    System.out.println("Invalid IP Address");
+                }
+
+                Log.d("GAT",subnet);
+
+                int i = 1;
+
+                // Define the port to check (e.g., port 80 for HTTP)
+                do{
+                    String ipAddress = subnet + "." + i;
                     try {
-                        socket = new Socket(serverIP, serverPort);
+                        socket = new Socket();
+                        socket.connect(new InetSocketAddress(ipAddress, serverPort), 1000); // 1000 ms timeout
+                        Log.v("NA","Found open port " + serverPort + " on host " + ipAddress);
                         out = new OutputStreamWriter(socket.getOutputStream());
                         writer = new BufferedWriter(out);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                        break;
+                    } catch (Exception e) {
+                        Log.v("NO","Can't connect open port " + serverPort + " on host " + ipAddress);
+                        try {
+                            socket.close();
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        }
                     }
-                }
+                    i++;
+                }while(!socket.isConnected());
 
 
             }
@@ -105,17 +129,10 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                            // socket = new Socket(serverIP, serverPort);
-                            //out = new OutputStreamWriter(socket.getOutputStream());
-                            // BufferedWriter writer = new BufferedWriter(out);
-
                             message = "open"+"đ"+url.getText().toString();
                             writer.write(message);
                             writer.newLine();
                             writer.flush();
-                            //writer.close();
-                            //out.close();
-                            //socket.close();
                             messageThread.interrupt();
 
                         } catch (IOException e) {
@@ -136,16 +153,10 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                            // socket = new Socket(serverIP, serverPort);
-                            //out = new OutputStreamWriter(socket.getOutputStream());
-                            // BufferedWriter writer = new BufferedWriter(out);
                             message = "play_stop";
                             writer.write(message);
                             writer.newLine();
                             writer.flush();
-                            //writer.close();
-                            //out.close();
-                            //socket.close();
                             messageThread.interrupt();
 
                         } catch (IOException e) {
@@ -165,16 +176,10 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                            // socket = new Socket(serverIP, serverPort);
-                            //out = new OutputStreamWriter(socket.getOutputStream());
-                            // BufferedWriter writer = new BufferedWriter(out);
                             message = "next";
                             writer.write(message);
                             writer.newLine();
                             writer.flush();
-                            //writer.close();
-                            //out.close();
-                            //socket.close();
                             messageThread.interrupt();
 
                         } catch (IOException e) {
@@ -193,16 +198,10 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                            // socket = new Socket(serverIP, serverPort);
-                            //out = new OutputStreamWriter(socket.getOutputStream());
-                            // BufferedWriter writer = new BufferedWriter(out);
                             message = "accept_all";
                             writer.write(message);
                             writer.newLine();
                             writer.flush();
-                            //writer.close();
-                            //out.close();
-                            //socket.close();
                             messageThread.interrupt();
 
                         } catch (IOException e) {
@@ -221,16 +220,10 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                            // socket = new Socket(serverIP, serverPort);
-                            //out = new OutputStreamWriter(socket.getOutputStream());
-                            // BufferedWriter writer = new BufferedWriter(out);
                             message = "full";
                             writer.write(message);
                             writer.newLine();
                             writer.flush();
-                            //writer.close();
-                            //out.close();
-                            //socket.close();
                             messageThread.interrupt();
 
                         } catch (IOException e) {
